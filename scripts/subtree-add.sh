1eec1ac967851d1a99b8a88f9fc4e2283539da53
kanosaki <kanosaki@gmail.com>
2016-10-02 15:11:44 +0900

    Add zprezto and enhancd as subtree.

--- scripts/subtree-add.sh
+++ scripts/subtree-add.sh
@@ -1,2 +1,8 @@
 git remote add subtree/zsh-pure https://github.com/sindresorhus/pure.git
 git subtree add --prefix=packages/zsh-pure subtree/zsh-pure master
+
+git remote add subtree/zsh-prezto https://github.com/sorin-ionescu/prezto.git
+git subtree add --prefix=packages/zsh-prezto subtree/zsh-prezto master
+
+git remote add subtree/zsh-enhancd https://github.com/b4b4r07/enhancd.git
+git subtree add --prefix=packages/zsh-enhancd subtree/zsh-enhancd master
--- scripts/subtree-pull.sh
+++ scripts/subtree-pull.sh
@@ -1,1 +1,3 @@
 git subtree pull --prefix=packages/zsh-pure subtree/zsh-pure master
+git subtree pull --prefix=packages/zsh-prezto subtree/zsh-prezto master
+git subtree pull --prefix=packages/zsh-enhancd subtree/zsh-enhancd master
