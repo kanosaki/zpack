f00b11c913ad24dc909b38d4630da63c9df7f5aa
kanosaki <kanosaki@gmail.com>
2016-10-02 15:08:45 +0900

    Automatically creates directory erased(ot not managed) by git

--- packages/zsh-core/pack.zsh
+++ packages/zsh-core/pack.zsh
@@ -3,6 +3,9 @@
 rm -rf $PACKDIR/zsh
 mkdir -p $PACKDIR/zsh
 cp -r autoload $PACKDIR/zsh
+if [[ ! -d fpath ]]; then
+  mkdir fpath
+fi
 cp -r fpath $PACKDIR/zsh
 cp -r init.zsh $PACKDIR/zsh
 cp -r install.sh $PACKDIR/zsh
--- packages/zsh-pure/pack.zsh
+++ packages/zsh-pure/pack.zsh
@@ -1,3 +1,4 @@
 
+mkdir -p $PACKDIR/zsh/fpath
 cp pure.zsh $PACKDIR/zsh/fpath/prompt_pure_setup
 cp async.zsh $PACKDIR/zsh/fpath/async
