2aacc3542af5cef241f136fdb11227bce9ef6751
kanosaki <kanosaki@gmail.com>
2016-10-17 01:37:00 +0900

    Fix tmux-powerline/install.sh

--- packages/tmux-powerline/install.sh
+++ packages/tmux-powerline/install.sh
@@ -7,4 +7,7 @@ fi
 cat $BASE_DIR/tmux/statusline.tmux.conf >> ~/.tmux.conf
 cp -r $BASE_DIR/tmux/powerline ~/.tmux/powerline
 cp $BASE_DIR/tmux/tmux-powerlinerc ~/.tmux
+if [[ -e ~/.tmux-powerlinerc ]]; then
+  rm -f ~/.tmux-powerlinerc
+fi
 ln -s  ~/.tmux/tmux-powerlinerc ~/.tmux-powerlinerc
